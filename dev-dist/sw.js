fec60134fda3c5617287b69b3247a968442b8811
Nikunj Usadadiya <nikunjusadadiya19@gmail.com>
2025-12-14 20:23:04 +0530

    Last Commit
    Krishyam -  logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.j90238ge1og"
+    "revision": "0.v07vo1bjag8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
